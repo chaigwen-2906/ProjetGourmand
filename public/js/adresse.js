cbc60fe50cc9b8ad620160ac4070078df05339cb
LEMOINE Gwenola <chaigwen29.06@gmail.com>
2020-01-31 13:46:47 +0100

    plandusite

--- public/js/adresse.js
+++ public/js/adresse.js
@@ -2,7 +2,6 @@ function envoieMessage() { // methode post j'envoie un message
 
 
     let apiUrl = " https://slack.com/api/chat.postMessage"; // se trouve dans api slack
-    //let token = "xoxb-927071157766-924428894820-Z8tY90fDbVaZ3vlosFc6jRIj"; //token = identifiant secret de slack
     let channel = "#bot";
     let token = creds.token;
     let prenon = document.getElementById("prenom").value; //pour mettre les deux
